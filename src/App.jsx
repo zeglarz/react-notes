83933a70936c29bd0e838ee3291e44d662d1ee76
Konrad Rudnicki <zeglarz@gmail.com>
2020-08-07 10:29:05 +0200

    v1.0 complete

---
+++ src/App.jsx
@@ -0,0 +1,66 @@
+import React, { useState, useRef, useEffect } from "react";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Note from "./components/Note";
+import CreateArea from "./components/CreateNote";
+import { ThemeProvider } from "styled-components";
+import useTheme from "./utils/useTheme";
+import { light, dark } from "./styles/theme";
+import { GlobalStyle } from "./styles/globalStyle";
+
+const App = () => {
+  const storage = window.localStorage;
+  const initialState = JSON.parse(storage.getItem("notes")) || [];
+  const [theme, toggleTheme] = useTheme();
+
+  const [notes, setNotes] = useState(initialState);
+  const [idxLastClicked, setTdxLastClicked] = useState(0);
+
+  useEffect(() => {
+    storage.setItem("notes", JSON.stringify(notes));
+  }, [storage, notes]);
+
+  const constraintsRef = useRef(null);
+
+  const addNote = newNote => {
+    setNotes(prevNotes => {
+      return [...prevNotes, newNote];
+    });
+  };
+
+  const deleteNote = id => {
+    setNotes(prevNotes => {
+      return prevNotes.filter((_, index) => {
+        return index !== id;
+      });
+    });
+  };
+
+  return (
+    <ThemeProvider theme={theme === "light" ? light : dark}>
+      <GlobalStyle />
+      <div ref={constraintsRef} style={{ width: "100vw", height: "100vh" }}>
+        <Header toggleSwitch={toggleTheme} isOn={theme === "dark"} />
+        <CreateArea onAdd={addNote} />
+        {notes.map((noteItem, index) => {
+          return (
+            <Note
+              key={index}
+              id={index}
+              title={noteItem.title}
+              content={noteItem.content}
+              onDelete={deleteNote}
+              dateTime={noteItem.createdAt}
+              idxLastClicked={idxLastClicked}
+              setTdxLastClicked={setTdxLastClicked}
+              constraintsRef={constraintsRef}
+            />
+          );
+        })}
+        <Footer />
+      </div>
+    </ThemeProvider>
+  );
+}
+
+export default App;
--- src/components/CreateNote.jsx
+++ src/components/CreateNote.jsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import AddIcon from '@material-ui/icons/Add';
 import Fab from '@material-ui/core/Fab';
 import Zoom from '@material-ui/core/Zoom';
-import getDate from './dateTime';
+import getDate from '../utils/dateTime';
 import { StyledForm } from '../styles/Form';
 
 function CreateNote(props) {
