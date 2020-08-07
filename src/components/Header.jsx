8988aa581b988121eab1ba5bab2504b2e9d7018d
Konrad Rudnicki <zeglarz@gmail.com>
2020-08-07 10:15:13 +0200

    switch comp

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,33 +1,16 @@
-import React from "react";
-import CreateIcon from "@material-ui/icons/Create";
-import { StyledHeader } from "../styles/Header";
-import { Switch } from "../styles/Switch";
-import { motion } from "framer-motion";
-import Moon from "../styles/moon";
-import Sun from "../styles/sun";
-
-const spring = {
-    type: "spring",
-    stiffness: 700,
-    damping: 30
-};
+import React from 'react';
+import CreateIcon from '@material-ui/icons/Create';
+import { StyledHeader } from '../styles/Header';
+import Switch from './Switch';
 
 function Header(props) {
     return (
         <StyledHeader>
             <h1>
-                <CreateIcon className="logo" />
+                <CreateIcon className="logo"/>
                 Rememberify
             </h1>
-            <Switch data-isOn={props.isOn} onClick={props.toggleSwitch}>
-                <motion.div
-                    layout
-                    transition={spring}
-                    whileTap={{ width: 33, textAlign: "center" }}
-                >
-                    {!props.isOn ? <Sun /> : <Moon />}
-                </motion.div>
-            </Switch>
+            <Switch {...props} />
         </StyledHeader>
     );
 }
