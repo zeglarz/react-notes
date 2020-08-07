ac77e0152d4675ee456b8284f0b4371c6a571843
Konrad Rudnicki <zeglarz@gmail.com>
2020-08-07 10:10:57 +0200

    swtch comp

---
+++ src/components/Switch.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { StyledSwitch } from '../styles/Switch';
+import { motion } from 'framer-motion';
+import Moon from '../styles/moon';
+import Sun from '../styles/sun';
+
+const spring = {
+    type: 'spring',
+    stiffness: 700,
+    damping: 30
+};
+
+const Switch = (props) => {
+    return (
+        <StyledSwitch data-isOn={props.isOn} onClick={props.toggleSwitch}>
+            <motion.div
+                layout
+                transition={spring}
+                whileTap={{ width: 33, textAlign: 'center' }}
+            >
+                {!props.isOn ? <Sun/> : <Moon/>}
+            </motion.div>
+        </StyledSwitch>);
+};
+
+export default Switch;
