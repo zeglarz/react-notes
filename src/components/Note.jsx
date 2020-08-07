bd449f99c56924904ced96bbea06e1181bfdaf88
Konrad Rudnicki <zeglarz@gmail.com>
2020-08-07 09:58:00 +0200

    note comp

---
+++ src/components/Note.jsx
@@ -0,0 +1,63 @@
+import React, { useState } from "react";
+import { DeleteOutlined } from "@material-ui/icons";
+import styled from "styled-components";
+import { StyledNote } from "../styles/Note";
+
+const DateTime = styled.span`
+  position: relative;
+  float: left;
+  font-size: 0.6em;
+  max-width: 6rem;
+  margin: 0px 0px;
+  padding-top: 1em;
+  opacity: 0.5;
+`;
+
+function Note(props) {
+    const [dragging, setDragging] = useState(false);
+    const [isClicked, setClicked] = useState(false);
+
+    function handleClick() {
+        props.onDelete(props.id);
+    }
+
+    return (
+        <StyledNote
+            drag
+            dragConstraints={props.constraintsRef}
+            dragMomentum={false}
+            className="note"
+            onDragStart={() => setDragging(true)}
+            onDragEnd={() => setDragging(false)}
+            style={{
+                zIndex: dragging
+                    ? 99999
+                    : isClicked
+                        ? 999999
+                        : props.idxLastClicked === props.id
+                            ? 99
+                            : 0
+            }}
+            onClick={() => {
+                setClicked(true);
+                console.log("clicked");
+                props.setTdxLastClicked(props.id);
+                console.log(props.idxLastClicked);
+            }}
+            onBlur={() => {
+                setClicked(false);
+                console.log("blurred");
+            }}
+            tabIndex="1"
+        >
+            <h1>{props.title}</h1>
+            <p>{props.content}</p>
+            <DateTime>Created on {props.dateTime}</DateTime>
+            <button onClick={handleClick}>
+                <DeleteOutlined />
+            </button>
+        </StyledNote>
+    );
+}
+
+export default Note;
